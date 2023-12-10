3238efaed549533df2a37689c707cd171a816180
max <1687723655@qq.com>
2023-12-10 13:22:56 +0800

    feat(stacks): finish the structure of array_stack

---
+++ stacks/array_stack.go
@@ -0,0 +1,61 @@
+package stacks
+
+import (
+	"fmt"
+	"github.com/246859/containers"
+	"github.com/246859/containers/internal"
+)
+
+var _ Stack[any] = (*ArrayStack[any])(nil)
+
+type ArrayStack[T any] struct {
+	s []T
+}
+
+func NewArrayStack[T any](capacity int) *ArrayStack[T] {
+	return &ArrayStack[T]{s: make([]T, 0, capacity)}
+}
+
+func (stack *ArrayStack[T]) Push(t ...T) {
+	stack.s = append(stack.s, t...)
+}
+
+func (stack *ArrayStack[T]) Pop() (_ T, _ bool) {
+	if stack.Size() == 0 {
+		return
+	}
+	peek := stack.s[len(stack.s)-1]
+	stack.s = stack.s[:len(stack.s)-1]
+	return peek, true
+}
+
+func (stack *ArrayStack[T]) Peek() (_ T, _ bool) {
+	if stack.Size() == 0 {
+		return
+	}
+	return stack.s[len(stack.s)-1], true
+}
+func (stack *ArrayStack[T]) Iterator() containers.IndexIterator[T] {
+	return internal.NewSliceIterator(stack.s)
+}
+
+func (stack *ArrayStack[T]) Values() []T {
+	var vals []T
+	it := stack.Iterator()
+	for it.Next() {
+		vals = append(vals, it.Value())
+	}
+	return vals
+}
+
+func (stack *ArrayStack[T]) Size() int {
+	return len(stack.s)
+}
+
+func (stack *ArrayStack[T]) Clear() {
+	stack.s = stack.s[:0:0]
+}
+
+func (stack *ArrayStack[T]) String() string {
+	return fmt.Sprintf("%+v", stack.s)
+}
---
+++ stacks/array_stack_test.go
@@ -0,0 +1,83 @@
+package stacks
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestArrayStack_Push_Pop(t *testing.T) {
+	data := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+
+	stack := NewArrayStack[int](32)
+	stack.Push(data[:5]...)
+	stack.Push(data[5:]...)
+
+	for i := len(data) - 1; i >= 0; i-- {
+		d := data[i]
+		pop, b := stack.Pop()
+		assert.True(t, b)
+		assert.Equal(t, d, pop)
+	}
+}
+
+func TestArrayStack_Push_Peek(t *testing.T) {
+	data := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+
+	stack := NewArrayStack[int](32)
+	stack.Push(data[:5]...)
+	stack.Push(data[5:]...)
+
+	for i := len(data) - 1; i >= 0; i-- {
+		d := data[i]
+		pop, b := stack.Peek()
+		assert.True(t, b)
+		assert.Equal(t, d, pop)
+
+		stack.Pop()
+	}
+
+}
+
+func TestArrayStack_Iterator(t *testing.T) {
+	data := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+
+	stack := NewArrayStack[int](32)
+	stack.Push(data[:5]...)
+	stack.Push(data[5:]...)
+
+	it := stack.Iterator()
+	for it.Next() {
+		assert.Equal(t, data[it.Index()], it.Value())
+	}
+
+	it.Reverse()
+	it.Rewind()
+
+	for it.Next() {
+		assert.Equal(t, data[it.Index()], it.Value())
+	}
+}
+
+func TestArrayStack_Values(t *testing.T) {
+	data := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+
+	stack := NewArrayStack[int](32)
+	stack.Push(data[:5]...)
+	stack.Push(data[5:]...)
+
+	values := stack.Values()
+
+	for i := 0; i < len(values); i++ {
+		assert.Equal(t, data[i], values[i])
+	}
+}
+
+func TestArrayStack_Size(t *testing.T) {
+	data := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+
+	stack := NewArrayStack[int](32)
+	stack.Push(data[:5]...)
+	assert.Equal(t, 5, stack.Size())
+	stack.Push(data[5:]...)
+	assert.Equal(t, 10, stack.Size())
+}
