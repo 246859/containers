6e103b43627ae1c0e7c0ce1e13db5369fe22cc6d
246859 <2633565580@qq.com>
2023-12-27 15:06:12 +0800

    docs(lists): update list comment

--- lists/list.go
+++ lists/list.go
@@ -8,21 +8,21 @@ import (
 type List[T any] interface {
 	// Get returns the elem at the given index
 	Get(i int) (T, bool)
-	// IndexOf returns the index of the given element, if not found, return -1
+	// IndexOf returns the index of first element that matches the given element. if not found, return -1
 	IndexOf(elem T, equal containers.Equal[T]) int
 	// Set replaces the elem at the given index
 	Set(i int, elem T)
-	// Add adds given elems to the list
+	// Add appends the given element to the end of the list
 	Add(elem ...T)
 	// Insert inserts the given elems into the list at the given index
 	Insert(i int, elem ...T)
 	// Remove removes the elem match given index from the list
 	Remove(i int)
-	// RemoveElem removes the given element from the list
+	// RemoveElem removes the first element matches the given from the list,
 	RemoveElem(elem T, equal containers.Equal[T])
 	// Contains check if list contains the given elements
 	Contains(elem T, equal containers.Equal[T]) bool
-	// Clone returns a copy of the original list
+	// Clone returns a value-copy of the original list, not the deep-copy list
 	Clone() List[T]
 	// Join joins the given list into the original list
 	Join(list List[T])
